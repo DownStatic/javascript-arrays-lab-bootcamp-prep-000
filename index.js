01787ba2d4f93007a79181070b37c4b5ed19e6d6
Christopher Stull <chris.stull@gmail.com>
2018-05-22 19:18:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,4 +26,11 @@ function prependKitten(name){
   return cats
 }
 
-function 
+function removeLastKitten(){
+  var cats = kittens.slice(0,kittens.length-1)
+  return cats
+}
+
+function removeFirstKitten(){
+  var cats = kittens.slice(1,kittens.length)
+}
